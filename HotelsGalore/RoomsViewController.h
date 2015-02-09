0eefd620f530b57c3f2987a272912a44864ce0c5
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-02-09 15:20:21 -0800

    Added RoomsViewController

---
+++ HotelsGalore/RoomsViewController.h
@@ -0,0 +1,13 @@
+//
+//  RoomsViewController.h
+//  HotelsGalore
+//
+//  Created by Rodrigo Carballo on 2/9/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoomsViewController : UIViewController
+
+@end
