2e6df15a59ba3b6f04f4ce3b0894311f09fac75e
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-02-09 15:02:08 -0800

    Cells not displaying .  Debugging

---
+++ HotelListViewController.m
@@ -0,0 +1,59 @@
+//
+//  HotelListViewController.m
+//  HotelsGalore
+//
+//  Created by Rodrigo Carballo on 2/9/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import "HotelListViewController.h"
+#import "AppDelegate.h"
+#import "Hotel.h"
+
+@interface HotelListViewController () <UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong,nonatomic) NSArray *hotels;
+
+@end
+
+@implementation HotelListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.tableView.dataSource = self;
+    AppDelegate *appDelegate = [UIApplication sharedApplication].delegate;
+    NSManagedObjectContext *context = appDelegate.managedObjectContext;
+    
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] initWithEntityName:@"Hotel"];
+    //you could create a predicate here
+    NSError *fetchError;
+    
+    NSArray *results = [context executeFetchRequest:fetchRequest error:&fetchError];
+    if (!fetchError) {
+        self.hotels = results;
+        [self.tableView reloadData];
+    }
+    
+    
+    // Do any additional setup after loading the view.
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (self.hotels) {
+        return self.hotels.count;
+    } else {
+        return 0;
+    }
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"HOTEL_CELL" forIndexPath:indexPath];
+    Hotel *hotel = self.hotels[indexPath.row];
+    cell.textLabel.text = hotel.name;
+    return cell;
+}
+
+
+
+
+@end
