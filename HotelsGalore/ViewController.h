6ed69ec0deea901f56354e4b6a83bda7c6ba3978
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-02-09 12:48:13 -0800

    Initial Commit

---
+++ HotelsGalore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HotelsGalore
+//
+//  Created by Rodrigo Carballo on 2/9/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
