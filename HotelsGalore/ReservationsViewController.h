3c993e7089c8af647c92d08402cfba65a6012d8f
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-02-10 13:40:36 -0800

    Added Reservation Controller as well as constraints

---
+++ HotelsGalore/ReservationsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReservationsViewController.h
+//  HotelsGalore
+//
+//  Created by Rodrigo Carballo on 2/10/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReservationsViewController : UIViewController
+
+@end
