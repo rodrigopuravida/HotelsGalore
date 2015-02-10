71c44b4d8637f52c182f8530d21095dc7d2df11f
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-02-10 14:28:28 -0800

    Created AvailabilityViewController

---
+++ HotelsGalore/AvailabilityViewController.h
@@ -0,0 +1,13 @@
+//
+//  AvailabilityViewController.h
+//  HotelsGalore
+//
+//  Created by Rodrigo Carballo on 2/10/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AvailabilityViewController : UIViewController
+
+@end
---
+++ HotelsGalore/AvailabilityViewController.m
@@ -0,0 +1,37 @@
+//
+//  AvailabilityViewController.m
+//  HotelsGalore
+//
+//  Created by Rodrigo Carballo on 2/10/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import "AvailabilityViewController.h"
+
+@interface AvailabilityViewController ()
+
+@end
+
+@implementation AvailabilityViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
