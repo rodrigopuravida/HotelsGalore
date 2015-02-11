916b11608e4c848957d2adcbfb0edbb933e17281
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-02-11 13:27:15 -0800

    Moving code from AppDelegate to CoreStack

--- HotelsGalore/CoreDataStack.h
+++ HotelsGalore/CoreDataStack.h
@@ -7,7 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
 
 @interface CoreDataStack : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+-(instancetype)initForTesting;
+
 
 @end
